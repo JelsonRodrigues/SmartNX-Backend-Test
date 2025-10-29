de01bdf87df984e83111f14e0a161ac19b6bf014
Jelson Rodrigues <jelsonrodrigues25@gmail.com>
2025-10-29 16:59:45 -0300

    chore: read dotenv vars

--- src/index.mjs
+++ src/index.mjs
@@ -1,6 +1,10 @@
 import express from 'express';
 import routes from './routes/index.mjs';
-import sequelize from './db/db.mjs';
+import dotenv from 'dotenv';
+import { initDB } from './db/index.mjs';
+
+// Load environment variables from .env file
+dotenv.config();
 
 const server = express();
 
