2e71971182ac9c4140985487ccad471e427ff681
Jelson Rodrigues <jelsonrodrigues25@gmail.com>
2025-10-30 20:34:48 -0300

    fix: deny auth from inactive user

--- src/routes/auth.mjs
+++ src/routes/auth.mjs
@@ -10,7 +10,7 @@ const router = Router();
 router.post('/api/v1/auth', 
   body("username").isString().isLength({min: 6, max: 64}).withMessage("username must be between 6 and 64 characters long"),  
   body("password").isString().isLength({min: 12, max: 128}).withMessage("password must be between 12 and 128 characters long"),
-  (request, response) => {
+  async (request, response) => {
     const result_of_validation = validationResult(request);
     
     if (!result_of_validation.isEmpty()) {
@@ -24,20 +24,22 @@ router.post('/api/v1/auth',
 
     const {username, password} = request.body;
     const  { User } = models;
-    User.findOne({where: {user_name: username}}).then((user) => {
-      if (!user) {
-        return response.status(401).send("Invalid username or password");
-      }
+    const user = await User.findOne({where: {user_name: username, is_active: true}});
 
-      if (!bcrypt.compareSync(password, user.password)) {
-        return response.status(401).send("Invalid username or password");
-      }
+    if (!user) {
+      return response.status(401).send("Invalid username or password");
+    }
 
-      const user_jwt_token = jwt.sign(
-        { user_id: user.id, user_name: user.user_name },
-        process.env.JWT_SECRET, { expiresIn: '1h' })
-      response.status(200).send({ message: "Authentication successful", token: user_jwt_token });
-    });
+    if (!bcrypt.compareSync(password, user.password)) {
+      return response.status(401).send("Invalid username or password");
+    }
+
+    const user_jwt_token = jwt.sign(
+      { user_id: user.id, user_name: user.user_name },
+      process.env.JWT_SECRET, { expiresIn: '1h' });
+    
+    return response.status(200).send({ message: "Authentication successful", token: user_jwt_token });
+
 });
 
 export default router;
